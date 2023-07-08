0694a536fb5aa7488e6518f7de846d3176bef3c5
Takaaki Sato <takaaki@demiquartz.jp>
2023-07-08 23:23:56 +0900

    Add version.hpp

---
+++ src/core/version.hpp
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * @brief
+ * Define the version of the library.
+ *
+ * @author
+ * Takaaki Sato
+ *
+ * @copyright @parblock
+ * (c) 2023, Demiquartz <info@demiquartz.jp>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @endparblock
+ */
+#ifndef STARLIGHT_CORE_VERSION_HPP
+#define STARLIGHT_CORE_VERSION_HPP
+
+namespace Starlight::Core::Version {
+
+/**
+ * @brief Name of the library.
+ */
+constexpr auto Name = "Starlight Core Library";
+
+/**
+ * @brief Major version number of the library.
+ */
+constexpr auto Major = 0;
+
+/**
+ * @brief Minor version number of the library.
+ */
+constexpr auto Minor = 0;
+
+/**
+ * @brief Patch version number of the library.
+ */
+constexpr auto Patch = 0;
+
+} // namespace Starlight::Core::Version
+
+#endif // STARLIGHT_CORE_VERSION_HPP
